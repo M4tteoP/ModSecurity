ed6a31a992f487a93792679418c9c9018cb3a75f
M4tteoP <pace.matteo96@gmail.com>
2022-08-18 10:19:57 +0200

    tweaks Pm - custom delimiter

--- src/operators/pm_from_file.cc
+++ src/operators/pm_from_file.cc
@@ -43,54 +43,34 @@ bool PmFromFile::isComment(const std::string &s) {
     return true;
 }
 
-// PmFromFile is currently not available for Wasm
-// Tweaking it as a Pm with a custom delimiter.
-// Example expected syntax: @PmFromFile delim:| htaccess|<script> <hello>|hi
 bool PmFromFile::init(const std::string &config, std::string *error) {
     std::istream *iss;
-    char delim;
-    std::string tmp;
 
-    if (m_param.compare(0, 6, "delim:") == 0 && m_param.size() > 7) {
-        delim=m_param.at(6);
+    if (m_param.compare(0, 8, "https://") == 0) {
+        Utils::HttpsClient client;
+        bool ret = client.download(m_param);
+        if (ret == false) {
+            error->assign(client.error);
+            return false;
+        }
+        iss = new std::stringstream(client.content);
     } else {
-        error->assign("Failed to find delimiter (e.g. 'delim:-') at:" + m_param);
-        delete iss;
-        return false;
+        std::string err;
+        std::string resource = utils::find_resource(m_param, config, &err);
+        iss = new std::ifstream(resource, std::ios::in);
+
+        if (((std::ifstream *)iss)->is_open() == false) {
+            error->assign("Failed to open file: " + m_param + ". " + err);
+            delete iss;
+            return false;
+        }
     }
 
-    m_param.erase(0,8);
-    iss = new std::istringstream(m_param);
-
-    while (std::getline(*iss, tmp, delim)) {
-            acmp_add_pattern(m_p, tmp.c_str(), NULL, NULL, tmp.length());
+    for (std::string line; std::getline(*iss, line); ) {
+        if (isComment(line) == false) {
+            acmp_add_pattern(m_p, line.c_str(), NULL, NULL, line.length());
+	}
     }
-
-    // if (m_param.compare(0, 8, "https://") == 0) {
-    //     Utils::HttpsClient client;
-    //     bool ret = client.download(m_param);
-    //     if (ret == false) {
-    //         error->assign(client.error);
-    //         return false;
-    //     }
-    //     iss = new std::stringstream(client.content);
-    // } else {
-    //     std::string err;
-    //     std::string resource = utils::find_resource(m_param, config, &err);
-    //     iss = new std::ifstream(resource, std::ios::in);
-
-    //     if (((std::ifstream *)iss)->is_open() == false) {
-    //         error->assign("Failed to open file: " + m_param + ". " + err);
-    //         delete iss;
-    //         return false;
-    //     }
-    // }
-
-    // for (std::string line; std::getline(*iss, line); ) {
-    //     if (isComment(line) == false) {
-    //         acmp_add_pattern(m_p, line.c_str(), NULL, NULL, line.length());
-	// }
-    // }
 
     while (m_p->is_failtree_done == 0) {
         acmp_prepare(m_p);
